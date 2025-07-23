c3c7def8082dcb7effe370497a74e8a2db3dd681
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-07-23 15:55:25 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const app = express();
 import cors from "cors";
 import  connectDB from './utils/db.js'
 import userRoute from './routes/user.routes.js';
+import companyRoute from './routes/company.route.js';
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js"
+
+
 import dotenv from 'dotenv';
 dotenv.config({});
 
@@ -16,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
     origin:'http//localhost:5173',
-    Credentials:true
+    credentials:true
 }
 app.use(cors(corsOptions)); 
 
@@ -24,6 +29,9 @@ const PORT = process.env.PORT || 3000;
 
 //api
 app.use('/api/v1/user', userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job",jobRoute)
+app.use("/api/v1/application",applicationRoute)
 
 app.listen(PORT,()=>{
     connectDB();
