eef94b824e6ebaa7ee55ad5f07d969b5da80f021
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-07-27 20:46:18 +0530

    fifth commit

---
+++ frontend/src/components/CategoryCarousely.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Category =[
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer",
+]
+const CategoryCarousely = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryCarousely
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousely from './CategoryCarousely'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
+      <CategoryCarousely/>
+      {/* <LatestJobs/> */}
+      {/* <Footer/> */}
     </div>
   )
 }
