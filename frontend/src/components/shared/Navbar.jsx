aee9dd75760bf7ce1aa63966c88a38f8ea19cf04
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-07-30 20:21:26 +0530

    seventh commit

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -4,22 +4,23 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { LogOut, User2 } from "lucide-react";
 import { Link, Links } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
-  const user = false;
+  const {user} = useSelector(store=>store.auth)
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
           <h1 className="text-2xl font-bold">
-            Job<span className="text-purple-400">Portal</span>
+            Job<span className="text-[#F83002]">Portal</span>
           </h1>
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li>Home</li>
-            <li>Jobs</li>
-            <li>Browse</li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/jobs'>Jobs</Link></li>
+            <li><Link to='/browse'>Browse</Link></li>
           </ul>
           {!user ? (
             <div className="flex items-center gap-2">
